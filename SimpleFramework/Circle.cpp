9d7dc5a2808129d8f1f788ced8f5a5578e940a33
JusticeAU <justin.etchell@gmail.com>
2023-02-01 14:22:24 +1100

    Add protection for Circles being on top of each other

--- SimpleFramework/Circle.cpp
+++ SimpleFramework/Circle.cpp
@@ -15,6 +15,7 @@ CollisionData Circle::CollisionWithCircle(Circle* shapeB)
 	if (distanceFromAToB < m_radius + shapeB->m_radius)
 	{
 		Vec2 displacement = shapeB->m_position - m_position;
+		if (distanceFromAToB == 0) displacement = Vec2(1, 0); // if the circles are on top of each other, we just decide that they will move away from each other on the X plane.
 		col.depth = m_radius + shapeB->m_radius - distanceFromAToB;
 		col.normal = glm::normalize(displacement);
 		col.worldPosition = m_position + col.normal * (m_radius - col.depth / 2);
