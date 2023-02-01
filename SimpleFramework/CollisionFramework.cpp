b6233b8914c5cbe71fd17c5420bfcc20c23ebafa
JusticeAU <justin.etchell@gmail.com>
2023-02-01 14:07:40 +1100

    Convert scene to list of shapes and run collisions against the lot

--- SimpleFramework/CollisionFramework.cpp
+++ SimpleFramework/CollisionFramework.cpp
@@ -3,23 +3,50 @@
 
 CollisionFramework::CollisionFramework()
 {
-
+	shapes.push_back(yourCircle);
 }
 
 void CollisionFramework::Update(float delta)
 {
 
 	if (leftMouseDown)
-		m_CircleA->m_position = cursorPos;
-	else if (rightMouseDown)
-		m_CircleB->m_position = cursorPos;
+		yourCircle->m_position = cursorPos;
+	
+	if (rightMouseDown && !rightDown)
+	{
+		rightDown = true;
+		shapes.push_back(new Circle(cursorPos, 1));
+	}
+	else if (!rightMouseDown && rightDown)
+		rightDown = false;
 
-	CollisionData col = m_CircleA->CollisionWithCircle(m_CircleB);
+	/*CollisionData col = m_CircleA->CollisionWithCircle(m_CircleB);
 	col.Resolve();
-	col.DebugDraw(*lines);
+	col.DebugDraw(*lines);*/
+
+	// Perform collision tests and resolution 1 pass against all shapes.
+	if (shapes.size() >= 2)
+	{
+		for (int a = 0; a < shapes.size(); a++)
+		{
+			for (int b = a + 1; b < shapes.size(); b++)
+			{
+				CollisionData col = shapes[a]->CollisionWithCircle(shapes[b]);
+				col.Resolve();
+				col.DebugDraw(*lines);
+			}
+
+		}
+	}
+
 
 	//Draw
-	m_CircleA->Draw(*lines);
-	m_CircleB->Draw(*lines);
+
+	for (auto s : shapes)
+	{
+		s->Draw(*lines);
+	}
+	/*m_CircleA->Draw(*lines);
+	m_CircleB->Draw(*lines);*/
 
 }
--- SimpleFramework/CollisionFramework.h
+++ SimpleFramework/CollisionFramework.h
@@ -1,13 +1,15 @@
 #include "Application.h"
 #include "Circle.h"
+#include <vector>
 
 class CollisionFramework : public Application
 {
 public:
 	CollisionFramework();
 
-	Circle* m_CircleA = new Circle({-5,0}, 2.0f, {1,0,0});
-	Circle* m_CircleB = new Circle({ 5,0 }, 1.5f, {0,1,0});
-
 	void Update(float delta) override;
+
+	Circle* yourCircle = new Circle({ -5,0 }, 2.0f, { 1,0,0 });
+	std::vector<Circle*> shapes;
+	bool rightDown = false;
 };
